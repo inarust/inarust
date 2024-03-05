bf68a17898644f7bba3c89d5a8476cf28ef8dc89
Rolly Maulana Awangga <awangga@gmail.com>
2024-03-06 06:46:36 +0700

    db pindah ke config

--- src/configs/config.rs
+++ src/configs/config.rs
@@ -1,14 +1,21 @@
+use mongodb::{Client, options::ClientOptions};
+use std::sync::Arc;
 pub struct AppConfig {
     pub server_address: String,
-    pub mongo_string:String,
+    pub arc_client:Arc<Client>,
     // ... other configuration parameters ...
 }
 
-pub fn load_config() -> AppConfig {
+pub async fn load_config() -> AppConfig {
+    // Set up the MongoDB connection options
+    let client_options = ClientOptions::parse("mongodb://localhost:27017").await.unwrap();
+    // Connect to the MongoDB server
+    let client = Client::with_options(client_options).unwrap();
+    let arc_client = Arc::new(client);
     // Logic to load configuration from a file, environment variables, etc.
     AppConfig {
         server_address: "0.0.0.0:3000".to_string(),
-        mongo_string:"mongodb://localhost:27017".to_string(),
+        arc_client:arc_client,
         // ... initialize other configuration parameters ...
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use mongodb::{Client, options::ClientOptions};
-use std::sync::Arc;
 mod routes;
 mod controllers;
 mod models;
@@ -9,17 +7,11 @@ mod configs;
 #[tokio::main]
 async fn main() {
     //Load config
-    let config = configs::load_config();
-    // Set up the MongoDB connection options
-    let client_options = ClientOptions::parse(&config.mongo_string).await.unwrap();
-    // Connect to the MongoDB server
-    let client = Client::with_options(client_options).unwrap();
-    let arc_client = Arc::new(client);
-    
+    let config = configs::load_config().await;
   
     // Start Server
     println!("Running on {}",&config.server_address);
     let listener = tokio::net::TcpListener::bind(&config.server_address).await.unwrap();
-    let app = routes::route(arc_client);
+    let app = routes::route(config.arc_client);
     axum::serve(listener, app).await.unwrap();
 }
